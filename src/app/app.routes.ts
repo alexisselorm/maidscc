23acd47268cc73c681ee6cd19252da1f80dc78df
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2024-04-26 15:53:06 +0200

    home placeholder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { UserTableComponent } from './user-table/user-table.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'users', component: UserTableComponent },
   { path: 'users/:id', component: UserTableComponent },
 ];
